624bb3cad7cffcccf3400bcf7c6c4b5614efafe1
“bcoispeau” <baudoin.coispeau@epfl.ch>
2023-08-28 16:06:04 +0200

    Header HTML & CSS done. Initialize React routes for Swap and Tokens page.

--- dex/src/components/Header.js
+++ dex/src/components/Header.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import Logo from "../moralis-logo.svg";
+import PolygonLogo from "../polygon-matic-logo.svg";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header>
+      <div className="leftH">
+        <img src={Logo} alt="Moralis Logo" className="logo" />
+        <div className="headerItem">Swap</div>
+        <div className="headerItem">Tokens</div>
+      </div>
+      <div className="rightH">
+        <div className="headerItem">
+          <img src={PolygonLogo} alt="Polygon Matic Logo" className="polygon" />
+          Polygon
+        </div>
+        <div className="connectButton">Connect</div>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
